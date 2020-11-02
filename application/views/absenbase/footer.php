27bf86f2d541a9944ba245b98b2c868a7f46f2dc
taufiqru <taufiq4games@gmail.com>
2020-11-03 06:12:23 +0700

    add

---
+++ application/views/absenbase/footer.php
@@ -0,0 +1,29 @@
+
+    <!-- JS -->
+    <script src="<?=base_url()?>plugins/jquery/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="<?=base_url()?>/dist/js/main.js"></script>
+    <script type="text/javascript">
+        // $(document).ready(function(){
+        //     $('#submit').on('click',function(){
+        //         var data = {
+        //             nama : $('#name').val(),
+        //             pangkat : $('#pangkat').val(),
+        //             keterangan : $('#keterangan').val(),
+        //             kondisi : $('#kondisi').val(),
+        //             alamat : $('#alamat').val()
+        //         }
+
+        //         var url = "<?=base_url()?>/absen/submit";
+        //         $.post(url,data,function(status,data){
+                    
+        //         });
+
+
+        //     });
+        // });
+    </script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
---
+++ application/views/absenbase/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pushansiber</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="<?=base_url()?>dist/fonts/material-icon/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=base_url()?>plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+    <link rel="stylesheet" href="<?=base_url()?>dist/css/AdminLTE.min.css">
+    <link rel="icon" href="<?=base_url()?>dist/images/favicon.ico">
+    <link rel="stylesheet" href="<?=base_url()?>dist/css/skins/_all-skins.min.css">
+    <!-- Main css -->
+    <link rel="stylesheet" href="<?=base_url()?>dist/css/style.css">
+
+
+    <script src="<?=base_url()?>plugins/jquery/jquery.min.js"></script>
+    <script src="<?=base_url()?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?=base_url()?>dist/js/adminlte.min.js"></script>
+</head>
+
+<body>
